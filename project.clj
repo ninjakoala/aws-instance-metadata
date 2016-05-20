b2749a189b58e022b912edb523f2b909b826ba5b
Neil Prosser <neil.prosser@gmail.com>
2016-05-20 22:22:52 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/ninjakoala/instance-metadata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.3.1"]
-                 [clj-http "1.0.1"]
+  :dependencies [[cheshire "5.6.1"]
+                 [clj-http "3.1.0"]
                  [com.cemerick/url "0.1.1"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.tobereplaced/lettercase "1.0.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}}
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2"]]}}
   :plugins [[lein-release "1.0.5"]]
   :lein-release {:deploy-via :clojars})
