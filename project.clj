6868c29b091efedaf33927b24d09e880ff2d658a
Neil Prosser <neil.prosser@gmail.com>
2017-06-16 21:01:25 +0100

    Update project config

--- project.clj
+++ project.clj
@@ -1,15 +1,22 @@
 (defproject com.ninjakoala/aws-instance-metadata "1.0.2-SNAPSHOT"
   :description "Handles querying local instance metadata in EC2 instances"
-  :url "https://github.com/ninjakoala/instance-metadata"
+  :url "https://github.com/ninjakoala/aws-instance-metadata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.6.3"]
-                 [clj-http "3.4.1"]
+  :dependencies [[cheshire "5.7.1"]
+                 [clj-http "3.6.1"]
                  [com.cemerick/url "0.1.1"]
                  [org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.logging "0.4.0"]
                  [org.tobereplaced/lettercase "1.0.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
                    :plugins [[lein-midje "3.2.1"]]}}
-  :plugins [[lein-release "1.0.5"]]
-  :lein-release {:deploy-via :clojars})
+  :aliases {"test" ["midje"]}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
