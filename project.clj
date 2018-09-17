1b4ad3b76e9147fe505b6760d87e2cf495f9881e
Neil Prosser <neil.prosser@gmail.com>
2018-09-17 14:08:08 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.8.0"]
-                 [clj-http "3.7.0"]
+                 [clj-http "3.9.1"]
                  [com.cemerick/url "0.1.1"]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.4.0"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.logging "0.4.1"]
                  [org.tobereplaced/lettercase "1.0.0"]]
-  :profiles {:dev {:dependencies [[midje "1.9.0"]]
+  :profiles {:dev {:dependencies [[midje "1.9.2"]]
                    :plugins [[lein-midje "3.2.1"]]}}
   :aliases {"test" ["midje"]}
   :release-tasks [["vcs" "assert-committed"]
